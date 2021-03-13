d0f64c7058982bb409ee6748bd5ceb7b11099745
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-03-13 12:18:58 +0100

    added -decimal flag

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ crc32 - calculate crc32 checksum
 
 ## Usage
 ```
-	crc32 [ filename ...]
+	crc32 [-decimal] [ filename ...]
 ```
 
 ## Description
@@ -15,12 +15,12 @@ read the content from stdin.
 to return crc32 checksum of specified files, type:
 ```sh
 $ crc32 main.go README.md
-1175033037 main.go
-2279994761 README.md
+aad0247b main.go
+5cbc3315 README.md
 ```
 to return crc32 checksum of stdin, type:
 
 ```sh
 $ cat main.go | crc32
-1175033037
+aad0247b
 ```
--- main.go
+++ main.go
@@ -10,23 +10,29 @@ import (
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: crc32 [inputfile ...]\n")
+	fmt.Fprintf(os.Stderr, "usage: crc32 [-decimal] [inputfile ...]\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
 
-func printCrc32(filename string, content []byte, printFileName bool) {
+func printCrc32(filename string, content []byte, decimal bool, printFileName bool) {
 	t := crc32.MakeTable(crc32.Castagnoli)
-	r := crc32.Checksum(content, t)
-	if printFileName {
-		fmt.Printf("%d %s\n", r, filename)
+	crc := crc32.Checksum(content, t)
+
+	if decimal {
+		fmt.Printf("%d", crc)
 	} else {
-		fmt.Printf("%d\n", r)
+		fmt.Printf("%x", crc)
 	}
+	if printFileName {
+		fmt.Printf(" %s", filename)
+	}
+	fmt.Printf("\n")
 }
 
 func main() {
 	flag.Usage = usage
+	decimal := flag.Bool("decimal", false, "print crc32 in decimal notation")
 	flag.Parse()
 
 	args := flag.Args()
@@ -36,13 +42,13 @@ func main() {
 			if err != nil {
 				log.Fatalf("[ERROR] failed to open file %s, %s", filename, err)
 			}
-			printCrc32(filename, content, true)
+			printCrc32(filename, content, *decimal, true)
 		}
 	} else {
 		content, err := ioutil.ReadAll(os.Stdin)
 		if err != nil {
 			log.Fatalf("[ERROR] failed to read from stdin, %s", err)
 		}
-		printCrc32("-", content, false)
+		printCrc32("-", content, *decimal, false)
 	}
 }
